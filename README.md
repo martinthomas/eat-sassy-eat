b4d70d8f1483fff270713a069b93567459c8862b
Martin Thomas <martin@martinthomas.net>
2022-01-08 17:26:53 -0600

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,33 @@
+ 
+
+
+> Open this page at [https://martinthomas.github.io/eat-sassy-eat/](https://martinthomas.github.io/eat-sassy-eat/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/martinthomas/eat-sassy-eat** and import
+
+## Edit this project ![Build status badge](https://github.com/martinthomas/eat-sassy-eat/workflows/MakeCode/badge.svg)
+
+To edit this repository in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/martinthomas/eat-sassy-eat** and click import
+
+## Blocks preview
+
+This image shows the blocks code from the last commit in master.
+This image may take a few minutes to refresh.
+
+![A rendered view of the blocks](https://github.com/martinthomas/eat-sassy-eat/raw/master/.github/makecode/blocks.png)
+
+#### Metadata (used for search, rendering)
+
+* for PXT/arcade
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ images.g.ts
@@ -0,0 +1,75 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Fushca the Fireball":return img`
+. . . 2 2 . . . . . . . . . . . 
+. . . . 2 2 . . . . . . . . . . 
+. . . . 2 2 2 . . . . 2 . . . . 
+. . . . 2 2 2 . . . 2 . . . . . 
+. . . . 2 2 2 2 . 2 2 . . . . . 
+. . . 2 2 2 2 2 2 2 2 . . . . . 
+. . . 2 2 2 2 2 2 2 2 2 . . . . 
+. . . 2 2 2 2 2 2 2 2 2 . . . . 
+. . 2 1 1 1 2 2 2 1 1 1 2 . . . 
+. . 2 2 1 f 1 2 1 f 1 2 2 . . . 
+. . 2 2 2 2 2 2 2 f 2 2 2 . . . 
+. . 2 2 2 2 2 2 2 2 2 2 2 . . . 
+. . 2 2 2 1 2 1 2 1 2 2 2 . . . 
+. . . 2 1 2 1 2 1 2 1 2 . . . . 
+. . . 2 2 2 2 2 2 2 2 . . . . . 
+. . . . . 2 2 2 2 2 . . . . . . 
+`;
+            case "image3":
+            case "Oliver the orange":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . 7 7 7 . . . . . 
+. . . . . . . . 7 . 7 . . . . . 
+. . . . . . . . 7 . . . . . . . 
+. . . . . 4 4 4 4 4 4 . . . . . 
+. . . . 4 4 4 4 4 4 4 4 . . . . 
+. . . 4 4 4 4 4 4 4 4 4 4 . . . 
+. . 4 4 4 4 4 4 4 4 4 4 4 4 . . 
+. . 4 4 4 4 4 4 4 4 4 4 4 4 . . 
+. . 4 4 4 4 4 4 4 4 4 4 4 4 . . 
+. . 4 4 4 4 4 4 4 4 4 4 4 4 . . 
+. . 4 4 4 4 4 4 4 4 4 4 4 4 . . 
+. . . 4 4 4 4 4 4 4 4 4 4 . . . 
+. . . . 4 4 4 4 4 4 4 4 . . . . 
+. . . . . 4 4 4 4 4 4 . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image2":
+            case "Sassy the snake":return img`
+. . . . f f f f . . . . . . . . 
+. . . . f 1 5 7 f f . . . . . . 
+. . . . f 5 5 7 7 f . . . . . . 
+. . . . f 7 7 7 7 f . . . . . . 
+. . . . f f f f f f . . . . . . 
+. . . . . . f 7 7 7 f f . . . . 
+. . . . . . . f 7 7 7 7 f f . . 
+. . . f f f . . f 7 7 7 7 7 f . 
+. . f 7 7 f . . . f f 7 7 7 7 f 
+. f 7 7 7 f . . . . . f f 7 7 f 
+. f 7 7 f . . . . . . . f 7 7 f 
+. f 7 7 7 f f . . f f f 7 7 f . 
+. f 7 7 7 7 7 f f 7 7 7 7 f . . 
+. . f 7 7 7 7 7 7 7 7 7 f . . . 
+. . . f f 7 7 7 f f f f . . . . 
+. . . . . f f f . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
